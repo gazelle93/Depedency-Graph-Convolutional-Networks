1e219e15588b9690e98e4e2a0c9f5cd4c7635d7a
Mingyou Sung <mingyousung@gmail.com>
2022-04-15 12:05:44 -0400

    Update DepGCN.py

--- DepGCN.py
+++ DepGCN.py
@@ -2,38 +2,16 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-class Dependency_GCN(nn.Module):
-    def __init__(self, in_dim, out_dim, dependency_list, num_layers=1 ,reverse_case=True):
-        super(Dependency_GCN, self).__init__()
+class Dependency_GCNLayer(nn.Module):
+    def __init__(self, in_dim, out_dim, dependency_list, reverse_case=True):
+        super(Dependency_GCNLayer, self).__init__()
         # dim: dimension of dependency weight
         # dependency_list: the entire dependency types
         # reverse_case (default=True): Considering not only the result of dependency representation but also the reversed dependency representation
         
-        """
-        - Text:
-            My dog likes eating sausage.
-            
-        - Universal dependencies: 
-            nmod:poss(dog-2, My-1)
-            nsubj(likes-3, dog-2)
-            root(ROOT-0, likes-3)
-            xcomp(likes-3, eating-4)
-            obj(eating-4, sausage-5)
-            
-        * Dependency can be presented as a directed graph
-                  likes
-                 /     \
-           (nsubj)     (xcomp)
-            |             |
-            dog         eating
-            |             |
-           (nmod:poss)  (obj)
-            |             |
-            My          sausage
-        """
         self.in_dim = in_dim
         self.out_dim = out_dim
-        self.num_layers = num_layers
+        self.relu = nn.ReLU()
         
         self.dependency_weight_list =[['self', nn.Linear(out_dim, out_dim)],['root', nn.Linear(out_dim, out_dim)]]
         self.reverse_case = reverse_case
@@ -83,16 +61,25 @@ class Dependency_GCN(nn.Module):
                 temp_tensor[cur_governor] += self.weights[cur_dependency+'_r'](_input[cur_dependent].T)
                 
         return temp_tensor
+    
+    def forward(self, _input, dependency_triples):
+        return self.relu(self.message_passing(_input, dependency_triples))
+    
+class Dependency_GCN(nn.Module):
+    def __init__(self, in_dim, out_dim, dependency_list, num_layers=1 ,reverse_case=True):
+        super(Dependency_GCN, self).__init__()
+        # dim: dimension of dependency weight
+        # dependency_list: the entire dependency types
+        # reverse_case (default=True): Considering not only the result of dependency representation but also the reversed dependency representation
+        self.num_layers = num_layers
+        self.gcn_layer = []
+        for i in range(num_layers):
+            self.gcn_layer.append(Dependency_GCNLayer(in_dim, out_dim, dependency_list,reverse_case))
 
     def forward(self, _input, dependency_triples):
-        # _input: tokenized input text representation in vector space
-        # dependency_triples: (dependent index, dependency, governor index)
-        # * dependent and governor index follows the index of _input
-        
-        h_ = self.message_passing(_input, dependency_triples)
-        
+        h_ = self.gcn_layer[0](_input, dependency_triples)
         if self.num_layers > 1:
-            for _ in range(self.num_layers-2):
-                h_ = self.message_passing(h_, dependency_triples)
-            
+            for i in range(self.num_layers-1):
+                h_ = self.gcn_layer[i+1](h_, dependency_triples)
+                
         return h_
